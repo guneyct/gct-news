613701f0bd84c64bf073621cd32a125bd115843b
G|C|T <120514341+guneyct@users.noreply.github.com>
2023-01-28 16:59:17 +0300

    Delete gct-news directory

--- gct-news/news.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `gct_newspaper`;
-CREATE TABLE `gct_newspaper` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `url` longtext,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
